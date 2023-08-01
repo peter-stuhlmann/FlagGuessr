cb265714c6ff56d17655f99bd4f78622350cce52
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2023-08-01 02:45:14 +0200

    Add GA tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import GoogleAnalytics from './utils/analytics';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -16,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
+      <GoogleAnalytics GA_TRACKING_ID={'G-DK9ZSQ5EVS'} />
       <body className={inter.className}>{children}</body>
     </html>
   );
